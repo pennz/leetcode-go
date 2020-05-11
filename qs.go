070b358b8aa8dc94579ecfc969568f2c07e939c6
Vincent Zhou <fireflysuccess@gmail.com>
2020-05-11 14:15:41 +0800

    quicksort: to be done

---
+++ qs.go
@@ -0,0 +1,19 @@
+package main
+
+func quickSort(nums []string, compareFunc func(string, string) bool) {
+	if len(nums) <= 1 {
+		return
+	}
+
+	pivot := partition(nums, compareFunc)
+	ls, rs := nums[:pivot], nums[pivot:]
+	quickSort(ls, compareFunc)
+	quickSort(rs, compareFunc)
+}
+func partition(nums []string, compareFunc func(string, string) bool) int {
+	pivot, pv := 0, nums[0]
+	if pv == "" {
+		pivot += 1
+	}
+	return pivot
+}
