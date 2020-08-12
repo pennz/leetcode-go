cbc7911c4936733a39135af4d81a2eafac735565
Vincent Zhou <fireflysuccess@gmail.com>
2020-08-12 11:54:19 +0800

    1145: added test
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- 1145.binary-tree-coloring-game_test.go
+++ 1145.binary-tree-coloring-game_test.go
@@ -13,7 +13,9 @@ func Test_btreeGameWinningMove(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{"basic", args{&TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}, 3, 3}, true},
+		{"basic_2", args{&TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}, 3, 1}, false},
+		{"basic_3", args{&TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, nil, nil}}, 3, 2}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
