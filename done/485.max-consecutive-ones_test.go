7f53d0dbb03c0eaec9eea671a1fcccdfe96a0ad4
Vincent Zhou <fireflysuccess@gmail.com>
2020-08-21 07:48:45 +0800

    485: test added back
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- done/485.max-consecutive-ones_test.go
+++ done/485.max-consecutive-ones_test.go
@@ -1,1 +1,24 @@
-Parser.Parse source file: target parser.ParseFile(): /Volumes/CaseSensitive/leetcode-go/todo/485.max-consecutive-ones.go:39:46: expected ';', found int (and 1 more errors)
+package main
+
+import "testing"
+
+func Test_findMaxConsecutiveOnes(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantCons int
+	}{
+		// TODO: Add test cases.
+		{"basic", args{[]int{1, 1, 1, 0, 1, 1}}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotCons := findMaxConsecutiveOnes(tt.args.nums); gotCons != tt.wantCons {
+				t.Errorf("findMaxConsecutiveOnes() = %v, want %v", gotCons, tt.wantCons)
+			}
+		})
+	}
+}
