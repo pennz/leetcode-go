32b70965cfb00ff799d949f64fbf96e556f2d74c
Vincent Zhou <fireflysuccess@gmail.com>
2020-08-20 21:50:43 +0800

    485: done and move to done
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- todo/485.max-consecutive-ones.go
+++ todo/485.max-consecutive-ones.go
@@ -1,4 +1,5 @@
 package main
+
 /*
  * @lc app=leetcode id=485 lang=golang
  *
@@ -16,40 +17,53 @@ package main
  *
  * Given a binary array, find the maximum number of consecutive 1s in this
  * array.
- * 
+ *
  * Example 1:
- * 
+ *
  * Input: [1,1,0,1,1,1]
  * Output: 3
  * Explanation: The first two digits or the last three digits are consecutive
  * 1s.
  * ⁠   The maximum number of consecutive 1s is 3.
- * 
- * 
- * 
+ *
+ *
+ *
  * Note:
- * 
+ *
  * The input array will only contain 0 and 1.
  * The length of input array is a positive integer and will not exceed 10,000
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-func findMaxConsecutiveOnes(nums []int) cons int {
+//  ✔ Accepted
+//  ✔ 41/41 cases passed (40 ms)
+//  ✔ Your runtime beats 77.05 % of golang submissions
+//  ✔ Your memory usage beats 77.25 % of golang submissions (6.4 MB)
+//  ✔ Accepted
+//  ✔ 41/41 cases passed (40 ms)
+//  ✔ Your runtime beats 77.05 % of golang submissions
+//  ✔ Your memory usage beats 96.52 % of golang submissions (6.4 MB)
+func findMaxConsecutiveOnes(nums []int) (cons int) {
 	cons = 0
 	foundC := 0
 
-	for v := range nums {
-		switch v ; {
-		case v == 0: foundC = 0;
-	case v == 1: foundC++;
-	default: panic("Found invavlid value: "+v);
-		}
-		if foundC > cons {
-			cons = foundC
+	for i := range nums {
+		if nums[i] == 1 {
+			foundC++
+		} else {
+			if foundC > cons {
+				cons = foundC
+			}
+			foundC = 0
 		}
 	}
-    return
+
+	if foundC > cons {
+		cons = foundC
+	} // coner case [1]
+	return
 }
+
 // @lc code=end
