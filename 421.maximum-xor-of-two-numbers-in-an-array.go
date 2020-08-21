ea37446871da6f657a087929f3824adc08847863
Vincent Zhou <fireflysuccess@gmail.com>
2020-08-21 08:02:13 +0800

    Added 421
    Signed-off-by: Vincent Zhou <fireflysuccess@gmail.com>

--- 421.maximum-xor-of-two-numbers-in-an-array.go
+++ 421.maximum-xor-of-two-numbers-in-an-array.go
@@ -1,3 +1,5 @@
+package main
+
 /*
  * @lc app=leetcode id=421 lang=golang
  *
@@ -15,27 +17,28 @@
  *
  * Given a non-empty array of numbers, a0, a1, a2, … , an-1, where 0 ≤ ai <
  * 2^31.
- * 
+ *
  * Find the maximum result of ai XOR aj, where 0 ≤ i, j < n.
- * 
+ *
  * Could you do this in O(n) runtime?
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: [3, 10, 5, 25, 2, 8]
- * 
+ *
  * Output: 28
- * 
+ *
  * Explanation: The maximum result is 5 ^ 25 = 28.
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
 
 // @lc code=start
 func findMaximumXOR(nums []int) int {
-    
+
 }
+
 // @lc code=end
---
+++ 421.maximum-xor-of-two-numbers-in-an-array_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func Test_findMaximumXOR(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMaximumXOR(tt.args.nums); got != tt.want {
+				t.Errorf("findMaximumXOR() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
